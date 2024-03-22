6dbca8b1470c06ff2cf538aabc66b0fd58a50b6d
bdurushia <b.durushia@gmail.com>
2024-03-22 11:30:14 -0500

    Created a class and moved stuff from the main method to clean things up. Fixed a readkey bug.

--- API-Exercise2-OpenWeather/Program.cs
+++ API-Exercise2-OpenWeather/Program.cs
@@ -1,35 +1,9 @@
 ﻿using API_Exercise2_OpenWeather;
 using Newtonsoft.Json.Linq;
 
-var client = new HttpClient();
-
-var appsettingsText = File.ReadAllText("appsettings.json");
-
-var apiKey = JObject.Parse(appsettingsText)["apiKey"].ToString();
-// var zipApiKey = JObject.Parse(appsettingsText)["zipApiKey"].ToString();
-
 int zipCode = 0;
 bool isZip = false;
 
-var openWeatherURL = "";
-var weatherResponse = "";
-
-var latitude = "";
-var longitude = "";
-var latLongUrl = "";
-
-var coordinateResponse = "";
-
-var cityName = "";
-var state = "";
-var temp = "";
-var feelsLike = "";
-var tempMin = "";
-var tempMax = "";
-var description = "";
-
-
-// Use a Try Catch if not a valid zip code
 while (!isZip)
 {
     Console.Clear();
@@ -40,36 +14,10 @@ while (!isZip)
         Console.Write("\n\tTo check the current weather forecast,\n\tEnter the zipcode for that area: ");
         isZip = int.TryParse(Console.ReadLine(), out zipCode);
 
-        openWeatherURL = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&appid={apiKey}&units=imperial";
-        // var zipWeatherURL = $"https://www.zipcodeapi.com/rest/{zipApiKey}/info.json/{zipCode}/degrees";
-
-        weatherResponse = client.GetStringAsync(openWeatherURL).Result;
-        // var zipWeatherResponse = client.GetStringAsync(zipWeatherURL).Result;
-
-        latitude = JObject.Parse(weatherResponse)["coord"]["lat"].ToString();
-        longitude = JObject.Parse(weatherResponse)["coord"]["lon"].ToString();
-
-        latLongUrl = $"http://api.openweathermap.org/geo/1.0/reverse?lat={latitude}&lon={longitude}&appid={apiKey}";
-
-        coordinateResponse = client.GetStringAsync(latLongUrl).Result;
-
-        cityName = JObject.Parse(weatherResponse)["name"].ToString();
-        state = JArray.Parse(coordinateResponse)[0]["state"].ToString();
-        temp = JObject.Parse(weatherResponse)["main"]["temp"].ToString();
-        feelsLike = JObject.Parse(weatherResponse)["main"]["feels_like"].ToString();
-        tempMin = JObject.Parse(weatherResponse)["main"]["temp_min"].ToString();
-        tempMax = JObject.Parse(weatherResponse)["main"]["temp_max"].ToString();
-        description = JObject.Parse(weatherResponse)["weather"][0]["description"].ToString();
-
-
-        Connecting.PrintConnection();
-
-        Console.WriteLine($"\n\tCurrent weather for:\n\t{cityName}, {state}");
-        Console.WriteLine($"\n\t{description}\n\tTemp: {temp}\u00B0F\n\tReal-Feel: {feelsLike}\u00B0F" +
-                          $"\n\tHi: {tempMax}\u00B0F\n\tLo: {tempMin}\u00B0F");
+        Weather.GetWeather(zipCode, isZip);
 
         Console.Write("\n\tPress 'Enter' to search again or 'ESC' to quit: ");
-        ConsoleKeyInfo keyStroke = Console.ReadKey();
+        ConsoleKeyInfo keyStroke = Console.ReadKey(true); // If 'true' is not in the readkey parameter, the ESC key deletes the first char of the next console.writeline
         if (keyStroke.Key == ConsoleKey.Enter)
         {
             isZip = false;
---
+++ API-Exercise2-OpenWeather/Weather.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_Exercise2_OpenWeather
+{
+    public class Weather
+    {
+        public static void GetWeather(int zipCode, bool isZip)
+        {
+            var client = new HttpClient();
+
+            var appsettingsText = File.ReadAllText("appsettings.json");
+
+            var apiKey = JObject.Parse(appsettingsText)["apiKey"].ToString();
+            // var zipApiKey = JObject.Parse(appsettingsText)["zipApiKey"].ToString();
+
+            var openWeatherURL = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&appid={apiKey}&units=imperial";
+            // var zipWeatherURL = $"https://www.zipcodeapi.com/rest/{zipApiKey}/info.json/{zipCode}/degrees";
+
+            var weatherResponse = client.GetStringAsync(openWeatherURL).Result;
+            // var zipWeatherResponse = client.GetStringAsync(zipWeatherURL).Result;
+
+            var latitude = JObject.Parse(weatherResponse)["coord"]["lat"].ToString();
+            var longitude = JObject.Parse(weatherResponse)["coord"]["lon"].ToString();
+
+            var latLongUrl = $"http://api.openweathermap.org/geo/1.0/reverse?lat={latitude}&lon={longitude}&appid={apiKey}";
+
+            var coordinateResponse = client.GetStringAsync(latLongUrl).Result;
+
+            var cityName = JObject.Parse(weatherResponse)["name"].ToString();
+            var state = JArray.Parse(coordinateResponse)[0]["state"].ToString();
+            var temp = JObject.Parse(weatherResponse)["main"]["temp"].ToString();
+            var feelsLike = JObject.Parse(weatherResponse)["main"]["feels_like"].ToString();
+            var tempMin = JObject.Parse(weatherResponse)["main"]["temp_min"].ToString();
+            var tempMax = JObject.Parse(weatherResponse)["main"]["temp_max"].ToString();
+            var description = JObject.Parse(weatherResponse)["weather"][0]["description"].ToString();
+
+            Connecting.PrintConnection();
+
+            Console.WriteLine($"\n\tCurrent weather for:\n\t{cityName}, {state}");
+            Console.WriteLine($"\n\t{description}\n\tTemp: {temp}\u00B0F\n\tReal-Feel: {feelsLike}\u00B0F" +
+                              $"\n\tHi: {tempMax}\u00B0F\n\tLo: {tempMin}\u00B0F");
+        }
+    }
+}
