e18fea8d107c7139edb028f0be62a3ab94e54684
bdurushia <b.durushia@gmail.com>
2024-03-21 21:31:38 -0500

    Finshed API exercise 2. Will polish the app up.

--- API-Exercise2-OpenWeather/Program.cs
+++ API-Exercise2-OpenWeather/Program.cs
@@ -5,6 +5,7 @@ var client = new HttpClient();
 var appsettingsText = File.ReadAllText("appsettings.json");
 
 var apiKey = JObject.Parse(appsettingsText)["apiKey"].ToString();
+// var zipApiKey = JObject.Parse(appsettingsText)["zipApiKey"].ToString();
 
 int zipCode = 0;
 bool isZip = false;
@@ -13,7 +14,8 @@ bool isZip = false;
 while (!isZip)
 {
     Console.Clear();
-    Console.WriteLine("Enter the zipcode of the city area you want to check the weather forecast for: ");
+    Console.WriteLine("\n\t\tWelcome to The Open Weather App!!!\n");
+    Console.Write("\nEnter the zipcode of the city area you want to check the weather forecast for: ");
     isZip = int.TryParse(Console.ReadLine(), out zipCode);
 
     if (isZip == false)
@@ -30,11 +32,24 @@ while (!isZip)
 
 Console.Clear();
 
+// 
+
 var openWeatherURL = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&appid={apiKey}&units=imperial";
+// var zipWeatherURL = $"https://www.zipcodeapi.com/rest/{zipApiKey}/info.json/{zipCode}/degrees";
 
 var weatherResponse = client.GetStringAsync(openWeatherURL).Result;
+// var zipWeatherResponse = client.GetStringAsync(zipWeatherURL).Result;
+
+var latitude = JObject.Parse(weatherResponse)["coord"]["lat"].ToString();
+var longitude = JObject.Parse(weatherResponse)["coord"]["lon"].ToString();
+
+var latLongUrl = $"http://api.openweathermap.org/geo/1.0/reverse?lat={latitude}&lon={longitude}&appid={apiKey}";
+
+var coordinateResponse = client.GetStringAsync(latLongUrl).Result;
+
 var cityName = JObject.Parse(weatherResponse)["name"].ToString();
+var state = JArray.Parse(coordinateResponse)[0]["state"].ToString();
 var temp = JObject.Parse(weatherResponse)["main"]["temp"].ToString();
 var feelsLike = JObject.Parse(weatherResponse)["main"]["feels_like"].ToString();
 
-Console.WriteLine($"City: {cityName}\nTemp: {temp}\u00B0F\nReal-Feel: {feelsLike}\u00B0F");
+Console.WriteLine($"\n\tCity: {cityName}, {state}\n\tTemp: {temp}\u00B0F\n\tReal-Feel: {feelsLike}\u00B0F");
